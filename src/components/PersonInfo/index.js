ca74ca3402e8336d39690f39e3fd0c619e8988d9
camila-firpo <camilafirpo2294@hotmail.com>
2021-12-10 09:09:35 -0300

    Info profile complete

--- src/components/PersonInfo/index.js
+++ src/components/PersonInfo/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import './index.css';
-<link
-  rel="stylesheet"
-  href="https://fonts.googleapis.com/icon?family=Material+Icons"
-/>
 
 export default class PersonInfo extends Component {
 	render() {
@@ -13,12 +14,26 @@ export default class PersonInfo extends Component {
 				<h1 className="name">Camila Firpo </h1>
 				<br/>
 				<div className="data">
-					<p>Celular: 099094726</p>
-					<p>Correo electrónico: camilafirpo2294@hotmail.com</p>
-					<p>Dirección: Samuel Blixen 4375</p>
+				<Typography variant="body1"	>
+					Cellphone: 099094726			
+				</Typography>
+				<Typography variant="body1"	>
+					E-mail: camilafirpo2294@hotmail.com
+				</Typography>
+					<LocationOnIcon/>
+					<Typography variant="body1"	>
+  						Home address: Malvin, Montevideo
+					</Typography>
+					<Link href="https://www.linkedin.com/in/camila-firpo/" target="_blank">
+						<LinkedInIcon />
+					</Link>
+					<br/>
+					<Link href="https://github.com/camila-firpo" target="_blank">
+						<GitHubIcon />
+					</Link>
+								
 				</div>
-				{/* <LinkedInIcon/>
-				<button className="button">LinkedInIcon</button> */}
+				
 			</div>)
 	}
 }
